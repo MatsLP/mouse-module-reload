8c417601b8299e056f0df640606b640b4b827b1e
Lukas Plogmacher <lukas.plogmacher@gmail.com>
2026-05-01 17:53:28 +0200

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 use std::process::Command;
 
-
-
-
-
-
-
 fn main() {
-    Command::new("modprobe").args(
-        ["--remove", "psmouse"]
-    ).status().expect("Could not unload psmouse");
+    Command::new("modprobe")
+        .args(["--remove", "psmouse"])
+        .status()
+        .expect("Could not unload psmouse");
 
-
-    Command::new("modprobe").args(
-        ["psmouse"]
-    ).status().expect("Could not load psmouse");
+    Command::new("modprobe")
+        .args(["psmouse"])
+        .status()
+        .expect("Could not load psmouse");
 }
